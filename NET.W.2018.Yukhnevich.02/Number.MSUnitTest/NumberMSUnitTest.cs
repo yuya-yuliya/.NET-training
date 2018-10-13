9a9a20174a3252dbcb52708643becc2aa9f82ad1
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-13 20:37:54 +0300

    Complete 2nd task

---
+++ NET.W.2018.Yukhnevich.02/Number.MSUnitTest/NumberMSUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Number;
+
+namespace Number.MSUnitTest
+{
+    [TestClass]
+    public class NumberMSUnitTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
+            "MSTestData.xml", 
+            "InsertNumber",
+            DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Number_InsertNumber_FromXML()
+        {
+            int numberSource = Convert.ToInt32(TestContext.DataRow["numberSource"]);
+            int numberIn = Convert.ToInt32(TestContext.DataRow["numberIn"]);
+            int startBit = Convert.ToInt32(TestContext.DataRow["startBit"]);
+            int endBit = Convert.ToInt32(TestContext.DataRow["endBit"]);
+            int expected = Convert.ToInt32(TestContext.DataRow["expected"]);
+
+            int result = Numbers.InsertNumber(numberSource, numberIn, startBit, endBit);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ NET.W.2018.Yukhnevich.02/Number/Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    /// <summary>
+    /// Class provides method to insert range of bits from other 32-bits number 
+    /// </summary>
+    public class Numbers
+    {
+
+        private const int BitsInByte = 8;
+
+        /// <summary>
+        /// Insert range of bits from one number to other
+        /// </summary>
+        /// <param name="numberSource">Source of insert bits</param>
+        /// <param name="numberIn">Number for insert in</param>
+        /// <param name="startBit">Start range of bits</param>
+        /// <param name="endBit">End range of bits</param>
+        /// <returns>Number with insert bits</returns>
+        public static int InsertNumber(int numberSource, int numberIn, int startBit, int endBit)
+        {
+            if (endBit < startBit)
+            {
+                throw new ArgumentException("Value of endBit mustn't be less than startBit");
+            }
+            if (endBit >= sizeof(int) * BitsInByte || startBit >= sizeof(int) * BitsInByte || endBit < 0 || startBit < 0 )
+            {
+                throw new ArgumentException("Start and end bits must be positive and less than " + sizeof(int) * BitsInByte);
+            }
+            for (int i = startBit; i <= endBit; i++)
+            {
+                numberSource = numberSource & ~(1 << i) | ((numberIn & 1) << i);
+                numberIn >>= 1;
+            }
+
+            return numberSource;
+        }
+    }
+}
