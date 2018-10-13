32169c5a0b03ceacf11d735227dce27c5ae865f3
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-13 20:45:26 +0300

    Add NUnit tests

---
+++ NET.W.2018.Yukhnevich.02/Number.NUnitTest/NumberNUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Number;
+
+namespace Number.NUnitTest
+{
+    [TestFixture]
+    public class NumberNUnitTest
+    {
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        public int Numbers_InsertNumber(int numberSource, int numberIn, int startBit, int endBit)
+        {
+            return Numbers.InsertNumber(numberSource, numberIn, startBit, endBit);
+        }
+    }
+}
