67f2263a29e4ba4b796c07f0bdd1af4ce0715b6f
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-14 17:38:47 +0300

    Complete addition to 3nd task
    Add ability to find time spent on finding next bigger number

--- NET.W.2018.Yukhnevich.03/NumberLibrary.Tests/BiggerNumberTests.cs
+++ NET.W.2018.Yukhnevich.03/NumberLibrary.Tests/BiggerNumberTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using NumberLibrary;
+using System.Diagnostics;
+using System;
 
 namespace NumberLibrary.Tests
 {
@@ -19,6 +21,29 @@ namespace NumberLibrary.Tests
         public int BiggerNumber_FindNextBiggerNumber(int number)
         {
             return BiggerNumber.FindNextBiggerNumber(number);
+        }
+
+        [Test]
+        public void BiggerNumber_FindNextBiggerNumberWithTime_OutParameter()
+        {
+            int number = 1234321;
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan findTime = new TimeSpan();
+            stopwatch.Start();
+            int nextBiggerNumber = BiggerNumber.FindNextBiggerNumberWithTime(number, out findTime);
+            stopwatch.Stop();
+            Assert.LessOrEqual(findTime, stopwatch.Elapsed);
+        }
+
+        [Test]
+        public void BiggerNumber_FindNextBiggerNumberWithTime_Tuple()
+        {
+            int number = 1234321;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Tuple<int, TimeSpan> nextBiggerAndTime = BiggerNumber.FindNextBiggerNumberWithTime(number);
+            stopwatch.Stop();
+            Assert.LessOrEqual(nextBiggerAndTime.Item2, stopwatch.Elapsed);
         }
     }
 }
