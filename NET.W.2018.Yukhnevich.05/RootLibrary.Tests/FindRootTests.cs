89672592b7dafec6f5d539307a34502f382c9cc2
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-14 15:07:41 +0300

    Complete 5th task

---
+++ NET.W.2018.Yukhnevich.05/RootLibrary.Tests/FindRootTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using RootLibrary;
+
+namespace RootLibrary.Tests
+{
+    [TestFixture]
+    public class FindRootTests
+    {
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void FindRoot_FindNthRoot(double a, int n, double delta, double expected)
+        {
+            Assert.AreEqual(expected, FindRoot.FindNthRoot(a, n, delta), delta);
+        }
+
+        [TestCase(8, 15, -7, -5)]
+        [TestCase(8, -15, 0.6, 0.1)]
+        public void FindRoot_FindNthRoot_ArgumentOutOfRangeException(double number, int degree, double delta, double expected)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FindRoot.FindNthRoot(number, degree, delta));
+        }
+
+    }
+}
---
+++ NET.W.2018.Yukhnevich.05/RootLibrary/FindRoot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RootLibrary
+{
+    /// <summary>
+    /// Class provides method to find Nth root of number with given precision
+    /// </summary>
+    public class FindRoot
+    {
+        /// <summary>
+        /// Find Nth root of number with given precision
+        /// </summary>
+        /// <remarks>
+        /// Use Newton's method
+        /// </remarks>
+        /// <param name="number">Number</param>
+        /// <param name="degree">Degree of root</param>
+        /// <param name="delta">Precision</param>
+        /// <returns>Root of number with given precision</returns>
+        public static double FindNthRoot(double number, int degree, double delta)
+        {
+            if (delta < 0)
+            {
+                throw new ArgumentOutOfRangeException("Delta must be positive number");
+            }
+
+            if (degree < 0)
+            {
+                throw new ArgumentOutOfRangeException("Degree must be positive integer number");
+            }
+
+            double xOld = 0, xNew = number / degree;
+            do
+            {
+                xOld = xNew;
+                xNew = (((degree - 1) * xOld) + (number / Math.Pow(xOld, degree - 1))) / degree;
+            }
+            while (Math.Abs(xNew - xOld) >= delta);
+            
+            return xNew;
+        }
+    }
+}
