5db40dfa510a3fc081e63a1e9ab031a0fb3beeed
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-14 17:44:12 +0300

    Revert "Revert "Complete addition to 2nd task""
    This reverts commit 21c1f519935a80cdf9fd4e5ad8231d9091b2f883.

--- NET.W.2018.Yukhnevich.03/NumberLibrary/BiggerNumber.cs
+++ NET.W.2018.Yukhnevich.03/NumberLibrary/BiggerNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace NumberLibrary
 {
@@ -14,7 +15,7 @@ namespace NumberLibrary
         /// Find next bigger number contains all digits from source number
         /// </summary>
         /// <param name="number">Source number</param>
-        /// <returns>First next bigger</returns>
+        /// <returns>Next bigger number</returns>
         public static int FindNextBiggerNumber(int number)
         {
             if (number <= 0)
@@ -40,6 +41,33 @@ namespace NumberLibrary
             }
 
             return -1;
+        }
+
+        /// <summary>
+        /// Find next bigger number contains all digits from source number with detecting time spent on finding
+        /// </summary>
+        /// <param name="number">Source number</param>
+        /// <param name="findTime">Time spent on finding</param>
+        /// <returns>Next bigger number</returns>
+        public static int FindNextBiggerNumberWithTime(int number, out TimeSpan findTime)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int nextBiggerNumber = FindNextBiggerNumber(number);
+            stopwatch.Stop();
+            findTime = stopwatch.Elapsed;
+            return nextBiggerNumber;
+        }
+
+        /// <summary>
+        /// Find next bigger number contains all digits from source number with detecting time spent on finding
+        /// </summary>
+        /// <param name="number">Source number</param>
+        /// <returns>Tuple of next bigger number and time spent on finding</returns>
+        public static Tuple<int, TimeSpan> FindNextBiggerNumberWithTime(int number)
+        {
+            int nextBiggerNumber = FindNextBiggerNumberWithTime(number, out TimeSpan time);
+            return new Tuple<int, TimeSpan>(nextBiggerNumber, time);
         }
 
         /// <summary>
