ed5ccc878d21fcb708a5b9588ce1ce995bc3d69d
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-14 13:23:53 +0300

    Complete 4th task

---
+++ NET.W.2018.Yukhnevich.04/FilterLibrary.Tests/FilterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using FilterLibrary;
+using System;
+
+namespace FilterLibrary.Tests
+{
+    [TestFixture]
+    public class FilterTests
+    {
+        [TestCase(7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17, ExpectedResult = new int[]{7, 70, 17})]
+        [TestCase(9, 11, 14, 5, 23, 11, ExpectedResult = new int[] { })]
+        [TestCase(1, -11, 13, 7, 5, 24, 5 ,-51, 17, ExpectedResult = new int[] {-11, 13, -51, 17 })]
+        public int[] Filter_FilterDigit(int digit, params int[] numbers)
+        {
+            return Filter.FilterDigit(digit, numbers);
+        }
+
+        [TestCase(10, 11, 5, 17)]
+        [TestCase(-1, 5, 7)]
+        public void Filter_FilterDigit_ArgumentException(int digit, params int[] numbers)
+        {
+            Assert.Throws(typeof(ArgumentException), () => { Filter.FilterDigit(digit, numbers); });
+        }
+    }
+}
---
+++ NET.W.2018.Yukhnevich.04/FilterLibrary/Filter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilterLibrary
+{
+    /// <summary>
+    /// Class provides method to filter number list in array of numbers containing the given digit
+    /// </summary>
+    public class Filter
+    {
+        private const int _minDigit = 0;
+        private const int _maxDigit = 9;
+        private const int _systemBase = 10;
+
+        /// <summary>
+        /// Filter number list in array of numbers containing the given digit
+        /// </summary>
+        /// <param name="digit">Filter digit</param>
+        /// <param name="numbers">Number list for filter</param>
+        /// <returns>Array of numbers containing filter digit</returns>
+        public static int[] FilterDigit(int digit, params int[] numbers)
+        {
+            if (!IsDigit(digit))
+            {
+                throw new ArgumentException("First parameter must be a 10 system base digit");
+            }
+
+            List<int> containDigitNumbers = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (HasDigit(digit, numbers[i]))
+                {
+                    containDigitNumbers.Add(numbers[i]);
+                }
+            }
+
+            return containDigitNumbers.ToArray();
+        }
+
+        /// <summary>
+        /// Check number is a digit
+        /// </summary>
+        /// <param name="number">Number for checking</param>
+        /// <returns>True if number is a digit, otherwise false</returns>
+        private static bool IsDigit(int number)
+        { 
+            if (number >= _minDigit && number <= _maxDigit)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check number is containing the given digit
+        /// </summary>
+        /// <param name="digit">Check digit</param>
+        /// <param name="number">Number for checking</param>
+        /// <returns>True if number is containing the digit, otherwise false</returns>
+        private static bool HasDigit(int digit, int number)
+        {
+            for (int num = number; num != 0; num /= _systemBase)
+            {
+                if (Math.Abs(num % _systemBase) == digit)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
