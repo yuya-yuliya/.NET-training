83bf8f663a92d90be8a38f7c880ac9c5222975bf
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-16 20:40:25 +0300

    Complete GCD task

---
+++ NET.W.2018.Yukhnevich.03/GCDLibrary.Tests/GCDTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using GCDLibrary;
+using System.Diagnostics;
+
+namespace GCDLibrary.Tests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+        [TestCase(4, 6, 8, 10, 20, 14, 16, ExpectedResult = 2)]
+        [TestCase(0, 2, ExpectedResult = 2)]
+        [TestCase(2, 0, ExpectedResult = 2)]
+        [TestCase(1, 3, ExpectedResult = 1)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        public int GetGCDEuclidean_test(int num1, int num2, params int[] numbers)
+        {
+            return GCD.GetGCDEuclidean(num1, num2, numbers);
+        }
+
+        [TestCase(4, 6, 8, 10, 20, 14, 16, ExpectedResult = 2)]
+        [TestCase(0, 2, ExpectedResult = 2)]
+        [TestCase(2, 0, ExpectedResult = 2)]
+        [TestCase(1, 3, ExpectedResult = 1)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        public int GetGCDBinary_test(int num1, int num2, params int[] numbers)
+        {
+            return GCD.GetGCDBinary(num1, num2, numbers);
+        }
+
+        [Test]
+        public void GetGCDEuclidean_outTime_test()
+        {
+            int[] numbers = new int[1000000];
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            GCD.GetGCDEuclidean(out TimeSpan time, numbers[0], numbers[1], numbers);
+            stopwatch.Stop();
+            Assert.LessOrEqual(time.Milliseconds, stopwatch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetGCDBinary_outTime_test()
+        {
+            int[] numbers = new int[1000000];
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            GCD.GetGCDBinary(out TimeSpan time, numbers[0], numbers[1], numbers);
+            stopwatch.Stop();
+            Assert.LessOrEqual(time.Milliseconds, stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ NET.W.2018.Yukhnevich.03/GCDLibrary/GCD.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace GCDLibrary
+{
+    /// <summary>
+    /// Class provides methods to find GCD of two or more numbers
+    /// </summary>
+    public class GCD
+    {
+        /// <summary>
+        /// Get GCD of two or more numbers using Euclidean algorithm
+        /// </summary>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        public static int GetGCDEuclidean(int num1, int num2, params int[] numbers)
+        {
+            return GetGCD(EuclideanGCD, num1, num2, numbers);
+        }
+
+        /// <summary>
+        /// Get GCD of two or more numbers using Binary algorithm
+        /// </summary>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        public static int GetGCDBinary(int num1, int num2, params int[] numbers)
+        {
+            return GetGCD(BinaryGCD, num1, num2, numbers);    
+        }
+
+        /// <summary>
+        /// Get GCD of two or more numbers using Euclidean algorithm
+        /// </summary>
+        /// <param name="time">Time spent on finding GCD</param>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        public static int GetGCDEuclidean(out TimeSpan time, int num1, int num2, params int[] numbers)
+        {
+            return GetGCD(EuclideanGCD, out time, num1, num2, numbers);
+        }
+
+        /// <summary>
+        /// Get GCD of two or more numbers using Binary algorithm
+        /// </summary>
+        /// <param name="time">Time spent on finding GCD</param>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        public static int GetGCDBinary(out TimeSpan time, int num1, int num2, params int[] numbers)
+        {
+            return GetGCD(BinaryGCD, out time, num1, num2, numbers);
+        }
+
+        /// <summary>
+        /// Get GCD of two or more numbers using some function
+        /// </summary>
+        /// <param name="gcdFinder">Function to find GCD of two positive numbers</param>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        private static int GetGCD(Func<int, int, int> gcdFinder, int num1, int num2, params int[] numbers)
+        {
+            List<int> numList = new List<int>(numbers);
+            numList.Add(num1);
+            int gcd = Math.Abs(num2);
+            for (int i = 0; i < numList.Count; i++)
+            {
+                gcd = gcdFinder(gcd, Math.Abs(numList[i]));
+            }
+            return gcd;
+        }
+
+        /// <summary>
+        /// <summary>
+        /// Get GCD of two or more numbers using some function
+        /// </summary>
+        /// <param name="gcdFinder">Function to find GCD of two positive numbers</param>
+        /// <param name="time">Time spent on finding GCD</param>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <param name="numbers">Other numbers</param>
+        /// <returns>GCD of all numbers</returns>
+        private static int GetGCD(Func<int, int, int> gcdFinder, out TimeSpan time, int num1, int num2, params int[] numbers)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int gcd = GetGCD(gcdFinder, num1, num2, numbers);
+            stopwatch.Stop();
+            time = stopwatch.Elapsed;
+            return gcd;
+        }
+
+        /// <summary>
+        /// Get GCD of two positive numbers using Euclidean algorithm
+        /// </summary>
+        /// <param name="num1">First positive number</param>
+        /// <param name="num2">Second positive number</param>
+        /// <returns>GCD of two positive numbers</returns>
+        private static int EuclideanGCD(int num1, int num2)
+        {
+            int temp;
+            while (num2 != 0)
+            {
+                temp = num2;
+                num2 = num1 % num2;
+                num1 = temp;
+            }
+            return num1;
+        }
+
+        /// <summary>
+        /// Get GCD of two positive numbers using Binary algorithm
+        /// </summary>
+        /// <param name="num1">First positive number</param>
+        /// <param name="num2">Second positive number</param>
+        /// <returns>GCD of two positive numbers</returns>
+        private static int BinaryGCD(int num1, int num2)
+        {
+            if (num1 == 0 || num1 == num2)
+            {
+                return num2;
+            }
+            if (num2 == 0)
+            {
+                return num1;
+            }
+            if ((num1 & 1) == 0)
+            {
+                if ((num2 & 1) == 0)
+                {
+                    return BinaryGCD(num1 >> 1, num2 >> 1) << 1;
+                }
+                else
+                {
+                    return BinaryGCD(num1 >> 1, num2);
+                }
+            }
+            if ((num2 & 1) == 0)
+            {
+                return BinaryGCD(num1, num2 >> 1);
+            }
+            if (num1 > num2)
+            {
+                return BinaryGCD((num1 - num2) >> 1, num2);
+            }
+            else
+            {
+                return BinaryGCD((num2 - num1) >> 1, num1);
+            }
+        }
+    }
+}
