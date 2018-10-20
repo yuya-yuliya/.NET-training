d85f181ea3dfe0efb45f7a413db9ca8b2075c0c9
yuya-yuliya <yuya.yuliya@gmail.com>
2018-10-20 23:12:00 +0300

    Complete polynomial task

---
+++ NET.W.2018.Yukhnevich.05/PolynomialLibrary.Tests/PolynomialTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace PolynomialLibrary.Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        static IEnumerable<Tuple<bool, Polynomial, Polynomial>> EqualsTestData()
+        {
+            double[] coefficients = { 0, 1, 2, 3, 4, 5 };
+            Polynomial polynomial1 = new Polynomial(coefficients);
+            Polynomial polynomial2 = new Polynomial(coefficients);
+            Polynomial polynomial3 = new Polynomial( 1, 2, 3, 4, 5, 6 );
+            Polynomial polynomial4 = new Polynomial( 0, 1, 2 );
+            Polynomial polynomial5 = new Polynomial();
+            yield return new Tuple<bool, Polynomial, Polynomial>(true, polynomial1, polynomial2);
+            yield return new Tuple<bool, Polynomial, Polynomial>(false, polynomial1, polynomial3);
+            yield return new Tuple<bool, Polynomial, Polynomial>(false, polynomial1, polynomial4);
+            yield return new Tuple<bool, Polynomial, Polynomial>(false, polynomial1, polynomial5);
+        }
+
+        [TestCaseSource(nameof(EqualsTestData))]
+        public void Equals_test(Tuple<bool, Polynomial, Polynomial> testTuple)
+        {
+            Assert.AreEqual(testTuple.Item1, testTuple.Item2.Equals(testTuple.Item3));
+        }
+
+        [TestCaseSource(nameof(EqualsTestData))]
+        public void GetHashCode_test(Tuple<bool, Polynomial, Polynomial> testTuple)
+        {
+            Assert.AreEqual(testTuple.Item1, testTuple.Item2.GetHashCode() == testTuple.Item3.GetHashCode());
+        }
+
+        [Test]
+        public void Addition_PolinomPolinom_test()
+        {
+            Polynomial pol1 = new Polynomial(1, 2, 3);
+            Polynomial pol2 = new Polynomial(2, 3, 4);
+            Polynomial expected = new Polynomial(3, 5, 7);
+
+            Assert.AreEqual(expected, pol1 + pol2);
+        }
+
+        [Test]
+        public void Subtraction_PolinomPolinom_test()
+        {
+            Polynomial pol1 = new Polynomial(1, 2, 3);
+            Polynomial pol2 = new Polynomial(2, 3, 4);
+            Polynomial expected = new Polynomial(-1, -1, -1);
+
+            Assert.AreEqual(expected, pol1 - pol2);
+        }
+
+        [Test]
+        public void Multiplication_ValuePolinom_test()
+        {
+            Polynomial pol = new Polynomial(1, 2, 3);
+            double value = 2;
+            Polynomial expected = new Polynomial(2, 4, 6);
+
+            Assert.AreEqual(expected, value * pol);
+        }
+
+        [Test]
+        public void Multiplication_PolinomValue_test()
+        {
+            Polynomial pol = new Polynomial(1, 2, 3);
+            double value = 2;
+            Polynomial expected = new Polynomial(2, 4, 6);
+
+            Assert.AreEqual(expected, pol * value);
+        }
+
+        [Test]
+        public void Multiplication_PolinomPolinom_test()
+        {
+            Polynomial pol = new Polynomial(1, 2);
+            Polynomial expected = new Polynomial(1, 4, 4);
+
+            Assert.AreEqual(expected, pol * pol);
+        }
+
+        [Test]
+        public void Division_PolinomValue_test()
+        {
+            Polynomial pol = new Polynomial(2, 4, 6);
+            double value = 2;
+            Polynomial expected = new Polynomial(1, 2, 3);
+
+            Assert.AreEqual(expected, pol / value);
+        }
+    }
+}
---
+++ NET.W.2018.Yukhnevich.05/PolynomialLibrary/Polynomial.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PolynomialLibrary
+{
+    /// <summary>
+    /// Class provides methods to create and work with Polynomial expressions
+    /// </summary>
+    public class Polynomial
+    {
+        private double[] coefficients;
+
+        /// <summary>
+        /// Create new object of class Polynomial with coefficients
+        /// </summary>
+        /// <param name="coefficients">Array of coefficients</param>
+        public Polynomial(params double[] coefficients)
+        {
+            this.coefficients = new double[coefficients.Length];
+            for (int i = 0;  i < coefficients.Length; i++)
+            {
+                this.coefficients[i] = coefficients[i];
+            }
+        }
+
+        /// <summary>
+        /// Calculate value of polynomial expression 
+        /// </summary>
+        /// <param name="x">Parameter value</param>
+        /// <returns>Value of polynom</returns>
+        public double Calculate(double x)
+        {
+            double value = 0;
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                value += coefficients[i] * Math.Pow(x, i);
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Get array of polynomial coefficients
+        /// </summary>
+        public double[] Coefficients
+        {
+            get
+            {
+                var coeff = new double[coefficients.Length];
+                Array.Copy(coefficients, coeff, coefficients.Length);
+                return coeff;
+            }
+        }
+
+        /// <summary>
+        /// Compare polynomials content
+        /// </summary>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>True if content is equal, folse otherwise</returns>
+        public static bool operator ==(Polynomial pol1, Polynomial pol2)
+        {
+            return pol1.Equals(pol2);
+        }
+
+        /// <summary>
+        /// Compare polynomials content
+        /// </summary>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>True if content is not equal, folse otherwise</returns>
+        public static bool operator !=(Polynomial pol1, Polynomial pol2)
+        {
+            return !pol1.Equals(pol2);
+        }
+
+        /// <summary>
+        /// Performs addition of two polynomials
+        /// </summary>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>New polynomial with addition results</returns>
+        public static Polynomial operator +(Polynomial pol1, Polynomial pol2)
+        {
+            return AddSubOperation((x, y) => x + y, pol1, pol2);
+        }
+
+        /// <summary>
+        /// Performs subtraction of two polynomials
+        /// </summary>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>New Polynomial with subtraction results</returns>
+        public static Polynomial operator -(Polynomial pol1, Polynomial pol2)
+        {
+            return AddSubOperation((x, y) => x - y, pol1, pol2);
+        }
+
+        /// <summary>
+        /// Perform operation with coefficients on the same positiond
+        /// </summary>
+        /// <param name="operation">Operation to perform</param>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>New polynomial with result of operation</returns>
+        private static Polynomial AddSubOperation(Func<double, double, double> operation, Polynomial pol1, Polynomial pol2)
+        {
+            double[] coeff1 = pol1.coefficients;
+            double[] coeff2 = pol2.coefficients;
+            double[] resultCoeff = new double[Math.Max(coeff1.Length, coeff2.Length)];
+            for (int i = 0; i < resultCoeff.Length; i++)
+            {
+                resultCoeff[i] = operation(i < coeff1.Length ? coeff1[i] : 0, i < coeff2.Length ? coeff2[i] : 0);
+            }
+            return new Polynomial(resultCoeff);
+        }
+
+        /// <summary>
+        /// Performs multiplication of two polynomials
+        /// </summary>
+        /// <param name="pol1">First polynom</param>
+        /// <param name="pol2">Second polynom</param>
+        /// <returns>New polynomial with multiplication results</returns>
+        public static Polynomial operator *(Polynomial pol1, Polynomial pol2)
+        {
+            double[] coeff1 = pol1.coefficients;
+            double[] coeff2 = pol2.coefficients;
+            double[] resultCoeff = new double[coeff1.Length + coeff2.Length - 1];
+            for (int i = 0; i < coeff1.Length; i++)
+            {
+                for (int j = 0; j < coeff2.Length; j++)
+                {
+                    resultCoeff[i + j] += coeff1[i] * coeff2[j];
+                }
+            }
+            return new Polynomial(resultCoeff);
+        }
+
+        /// <summary>
+        /// Performs multiplication of polynomial and constant value
+        /// </summary>
+        /// <param name="value">Value to multiply</param>
+        /// <param name="pol">Polinomial</param>
+        /// <returns>New polynomial with multiplication results</returns>
+        public static Polynomial operator *(double value, Polynomial pol)
+        {
+            return CoefficientsModify((x) => value * x, pol);
+        }
+
+        /// <summary>
+        /// Performs multiplication of polynomial and constant value
+        /// </summary>
+        /// <param name="pol">Polinomial</param>
+        /// <param name="value">Value to multiply</param>
+        /// <returns>New polynomial with multiplication results</returns>
+        public static Polynomial operator *(Polynomial pol, double value)
+        {
+            return value * pol;
+        }
+
+        /// <summary>
+        /// Performs division of polynomial on constant value
+        /// </summary>
+        /// <param name="pol">Polinomial</param>
+        /// <param name="value">Value to divide</param>
+        /// <returns>New polynomial with divition results</returns>
+        public static Polynomial operator /(Polynomial pol, double value)
+        {
+            return CoefficientsModify((x) => x / value, pol);
+        }
+
+        /// <summary>
+        /// Perform operation with coefficients of polynomial
+        /// </summary>
+        /// <param name="operation">Operation on coefficients</param>
+        /// <param name="pol">Polynomial</param>
+        /// <returns>New polynomial with operation results</returns>
+        private static Polynomial CoefficientsModify(Func<double, double> operation, Polynomial pol)
+        {
+            double[] oldCoeff = pol.coefficients;
+            double[] resultCoeff = new double[oldCoeff.Length];
+            for (int i = 0; i < resultCoeff.Length; i++)
+            {
+                resultCoeff[i] = operation(oldCoeff[i]);
+            }
+            return new Polynomial(resultCoeff);
+        }
+
+        /// <summary>
+        /// Compare contents of current polynomial and some object
+        /// </summary>
+        /// <param name="obj">Object to compare</param>
+        /// <returns>True if contents of current polynomial and object are equal, false otherwise</returns>
+        public override bool Equals(object obj)
+        {
+            if ((object)this == obj)
+            {
+                return true;
+            }
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Polynomial polynomial = (Polynomial)obj;
+            return coefficients.SequenceEqual(polynomial.coefficients);
+        }
+
+        /// <summary>
+        /// Get hash code of current object
+        /// </summary>
+        /// <returns>Object hash code</returns>
+        public override int GetHashCode()
+        {
+            int hash = 1;
+            int hashBase = 31;
+            unchecked
+            {
+                foreach (int coefficient in coefficients)
+                {
+                    hash = hash * hashBase + coefficient;
+                }
+            }
+            return hash;
+        }
+
+        /// <summary>
+        /// Get string format of current polynomial
+        /// </summary>
+        /// <returns>Polynomial string</returns>
+        public override string ToString()
+        {
+            StringBuilder polynomStrBuilder = new StringBuilder();
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                if (i == 0)
+                {
+                    polynomStrBuilder.Append($"{coefficients[i]}");
+                }
+                else
+                {
+                    polynomStrBuilder.Append($"+{coefficients[i]}x^{i}");
+                }
+            }
+            return polynomStrBuilder.ToString();
+        }
+    }
+}
